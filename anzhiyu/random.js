bf59bff76e4f366e9c2146045316634375041276
bkskys <8888860@gmail.com>
2025-06-28 12:51:18 +0800

    Site updated: 2025-06-28 12:51:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/26/这是一篇新的博文/","2025/06/26/hello-world/"];function toRandomPost(){
+var posts=["2025/06/28/第一篇笔记（测试）/","2025/06/26/这是一篇新的博文/","2025/06/27/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
